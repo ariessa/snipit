337eda179376556c8cfa62e846a7f158b68d8f56
ariessa <ariessa@users.noreply.github.com>
2024-06-29 02:12:44 +0800

    Change db from sqlite to postgresql

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "snipIt_production"
+  # config.active_job.queue_name_prefix = "snip_it_production"
 
   config.action_mailer.perform_caching = false
 
