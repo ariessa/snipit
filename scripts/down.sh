15d9a5f568f5b3e514876ec059fa74fb1eb3c27b
ariessa <ariessa@users.noreply.github.com>
2024-07-03 03:49:39 +0800

    Update down script

--- scripts/down.sh
+++ scripts/down.sh
@@ -15,8 +15,5 @@ sudo rm -rf postgres || true
 # Check which process is running on port 5432 and kill the process by PID
 sudo kill -9 $(sudo lsof -i :5432 | awk 'NR==2{print $2}') || true
 
-# # Delete package-lock.json file
-# sudo rm -f app/package-lock.json || true
-
-# # Delete node_modules folder
-# sudo rm -rf app/node_modules || true
+# Check which process is running on port 6379 and kill the process by PID
+sudo kill -9 $(sudo lsof -i :6379 | awk 'NR==2{print $2}') || true
