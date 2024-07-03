aea6451f6a550afb6509c8c51567daa56c8a5bc1
ariessa <ariessa@users.noreply.github.com>
2024-07-03 23:59:32 +0800

    Add integration tests for links, users, and views

---
+++ test/integration/users_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class UsersTest < ActionDispatch::IntegrationTest
+    test "guest user" do
+        get links_path
+        assert_select "a", "Sign Up"
+        assert_select "a", "Login"
+    end
+    test "user can be logged in" do
+        sign_in users(:one)
+        get links_path
+        assert_select "a", "Profile"
+        assert_select "button", "Log Out"
+    end
+end
