d0def75660b28f942b4e8028d861fa5f8743667f
ariessa <ariessa@users.noreply.github.com>
2024-07-04 10:11:15 +0800

    Put middleware in application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+require 'ipinfo-rails'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -15,6 +17,7 @@ module SnipIt
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.middleware.use(IPinfoMiddleware, { token: ENV['GMAPS_API_KEY'] })
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative "application"
 
-require 'ipinfo-rails'
-config.middleware.use(IPinfoMiddleware, {token: ENV['GMAPS_API_KEY']})
-
 # Initialize the Rails application.
 Rails.application.initialize!
