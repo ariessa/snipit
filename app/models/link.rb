39af62c21fec1b38e615a1c4214443262ad9f41e
ariessa <ariessa@users.noreply.github.com>
2024-07-16 02:00:59 +0800

    Validate url to allow only those with status code 200 to be added inside db

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,6 +3,7 @@ class Link < ApplicationRecord
     has_many :views, dependent: :destroy
     scope :recent_first, -> { order(created_at: :desc) }
     validates :url, presence: true, format: { with: URI::regexp(%w[http https]), message: 'must be a valid URL' }
+    validate :url_exists
 
     after_save_commit if: :url_previously_changed? do
         MetadataJob.perform_async(to_param)
@@ -27,4 +28,21 @@ class Link < ApplicationRecord
     def views_by_date_range(start_date, end_date)
         views.where(created_at: start_date..end_date)
     end
+
+    def url_exists
+        return unless url.present?
+    
+        url = URI.parse(self.url)
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = (url.scheme == 'https')
+    
+        begin
+          response = http.head(url.request_uri)
+          unless response.code.to_i == 200
+            errors.add(:url, "must be reachable (HTTP status #{response.code})")
+          end
+        rescue StandardError => e
+          errors.add(:url, "could not be validated: #{e.message}")
+        end
+      end
 end
